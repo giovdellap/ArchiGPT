6ecf208c1c9e7f6bb52796a1e0a41ffb270b7448
eugeniofacciolo31@outlook.com <eugeniofacciolo31@outlook.com>
2024-05-08 12:04:26 +0200

    dotenv in requirements.txt

--- ProjPP/src/backend/config.py
+++ ProjPP/src/backend/config.py
@@ -1,12 +1,11 @@
-#from dotenv import load_dotenv
+from dotenv import load_dotenv
 import os
 from openai import OpenAI
 
-#load_dotenv()
+load_dotenv()
 
 
 class ApplicationConfig:
-    #OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
-    OPENAI_API_KEY_G = "sk-proj-uoPd7wzdubIrsFdGwVDDT3BlbkFJRJIzt4ImRPIvvMhy9ShK"
-    OPENAI_API_KEY_E = ""
-    CLIENT = OpenAI(api_key = OPENAI_API_KEY_G)
+    OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+    #OPENAI_API_KEY_G = "sk-proj-uoPd7wzdubIrsFdGwVDDT3BlbkFJRJIzt4ImRPIvvMhy9ShK"
+    CLIENT = OpenAI(api_key = OPENAI_API_KEY)
