28f185563ed976c4b32cfcf7db5531249aa5815e
eugeniofacciolo31@outlook.com <eugeniofacciolo31@outlook.com>
2024-05-09 16:35:22 +0200

    add DataStructures Solo Generator Factory Assistant

--- ProjPP/src/backend/api_reference/assistants/assistantFactory.py
+++ ProjPP/src/backend/api_reference/assistants/assistantFactory.py
@@ -15,6 +15,8 @@ class AssistantFactory:
             self.set_assets_CLG()
         elif name == 'Endpoints Solo Generator':
             self.set_assets_ESG()
+        elif name == 'DataStructures Solo Generator':
+            self.set_assets_DSG()
         else:
             raise ValueError("Invalid assistant name")
 
@@ -49,5 +51,16 @@ class AssistantFactory:
         self.instructions_path = ESG_path +"/instructions.txt"
         self.tools = [{"type": "code_interpreter"},{"type": "file_search"}]
         self.tool_resources_path = { "code_interpreter": { "file_paths": RAG_files_paths }, "file_search": { "vector_store_paths": vector_store_paths , "vector_store_name" : "ESG_VS" }
+    }
+        
+    def set_assets_DSG(self):
+        DSG_path = self.resources_path + "/DSG"
+        RAG_path = DSG_path + "/RAG"
+        RAG_files_paths = [RAG_path + '/EXAMPLE 1 - source file.py', RAG_path + '/EXAMPLE 2 - source files.zip']
+        vector_store_paths = [RAG_path + '/EXAMPLE 1 - endpoints.txt', RAG_path + '/EXAMPLE 2 - endpoints.txt', RAG_path + '/EXAMPLE 1 - result.txt', RAG_path + '/EXAMPLE 2 - result.txt']
+
+        self.instructions_path = DSG_path +"/instructions.txt"
+        self.tools = [{"type": "code_interpreter"},{"type": "file_search"}]
+        self.tool_resources_path = { "code_interpreter": { "file_paths": RAG_files_paths }, "file_search": { "vector_store_paths": vector_store_paths , "vector_store_name" : "DSG_VS" }
     }
     
