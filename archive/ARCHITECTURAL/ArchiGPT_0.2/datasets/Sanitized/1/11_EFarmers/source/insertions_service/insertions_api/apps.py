05f334e1aad22bab081811c11c463bba80c0912b
giovdellap <giovdellap@gmail.com>
2024-05-14 18:12:23 +0200

    cleanup commit 2

--- archive/ARCHITECTURAL/ArchiGPT_0.2/datasets/Sanitized/1/11_EFarmers/source/insertions_service/insertions_api/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class InsertionsApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'insertions_api'
--- archive/ARCHITECTURAL/ArchiGPT_0.2/modules/ProjPP/V1/Sources/V1/21-NotFarmFromHome/source/NFFH-farmer-FE/src/app/utils/stringmakeup.ts
+++
@@ -1,7 +0,0 @@
-export function modifyString(str: string) {
-    return str.replaceAll('_', ' ')
-}
-
-export function revertString(str: string) {
-    return str.replaceAll(' ', '_')
-}
