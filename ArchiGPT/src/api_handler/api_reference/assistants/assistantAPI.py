550c5761396ff934ad8361fab6c532accfd0ce58
eugeniofacciolo31@outlook.com <eugeniofacciolo31@outlook.com>
2024-07-08 12:13:05 +0200

    add doc for presentation

--- ArchiGPT/src/api_handler/api_reference/assistants/assistantAPI.py
+++ ArchiGPT/src/api_handler/api_reference/assistants/assistantAPI.py
@@ -33,6 +33,7 @@ def creationHandler(assistantObj, req_ci, req_vs):
             if len(req_vs) > 0:
                 file_batch_assistant = uploadFileBatchesVectorStore(vector_store, req_vs)
             tool_resources['file_search']['vector_store_ids'] = [vector_store.id]
+            
         assistant = current_app.config['CLIENT'].beta.assistants.create(
             instructions=instructions_text,
             name=assistantObj.name,
